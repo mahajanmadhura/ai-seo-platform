7756a5ff6f79faf638a68f5081b9e6a54da9d6f9
Abduldiyan Deshmukh <abduldiyandeshmukh2005@gmail.com>
2026-07-27 11:15:04 +0530

    Updated url in config to better suit admin-feature functionality

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("api/auth/", include("accounts.urls")),
     path('api/v1/payments/', include('payments.urls')),
     path("api/v1/audits/",include("audits.urls")),
+    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/websites/', include('websites.urls')),
+    path('api/v1/system/', include('system.urls')),
+    path('api/v1/ai/', include('ai_engine.urls')),
+    path('api/v1/support/', include('support.urls')),
 ]
