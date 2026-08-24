573923f51f4bcb2c426fa243db5135c21144e408
Abduldiyan Deshmukh <abduldiyandeshmukh2005@gmail.com>
2026-08-24 16:57:15 +0530

    Removed static file connection between backend and the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    assetsDir: 'static', 
     chunkSizeWarningLimit: 1200,
     rollupOptions: {
       output: {
